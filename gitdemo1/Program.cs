4206c831126bd5ca9aa5bfcbcf9cde8886f9261e
vamshi <vamshibondala888@gmail.com>
2021-04-02 19:43:08 +0530

    Add project files.

---
+++ gitdemo1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitdemo1
+{
+    class class1
+    {
+
+        public void hello()
+        {
+            Console.WriteLine("this is the the method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            class1 c1 = new class1();
+            c1.hello();
+            Console.WriteLine("hello wolrd");
+            Console.ReadLine();
+        }
+      
+    }
+    
+}
